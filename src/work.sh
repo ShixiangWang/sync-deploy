c7020df43fef60c3cc46e6b117f4f681870165cb
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2018-03-30 17:37:04 +0800

    Modify sync deploy function

--- src/work.sh
+++ src/work.sh
@@ -1,15 +1,27 @@
-#PBS -l nodes=1:ppn=10
+#PBS -l walltime=02:30:00
+#PBS -l nodes=1:ppn=28
 #PBS -S /bin/bash
+#PBS -l mem=50gb
+
 #PBS -j oe
 #PBS -q normal_3
 
 # Please set PBS arguments above
-# This job's working directory
-cd ~/test
+# This job's working directory (You must set this)
+cd ~/wangshx/projects/biotools/ensembl-vep
 
-# Following are commands
-sleep 20
-echo "Thi mission is run successfully!!" > ~/test/result.txt
+# Following are commands/scripts you want to run
+# If you do not know how to set this, please check README.md file 
 
-# call Rscripts
-Rscript ~/test/test.R > ~/test/result2.txt
+# load perl and activate conda env
+
+module load apps/perl/5.22.1
+source activate py2
+
+# run ensembl-vep
+./vep --cache --offline --pick --symbol --species homo_sapiens --assembly GRCh38 --format vcf --terms SO \
+--fasta ~/wangshx/annotation/reference/hg38.fa --dir_plugins ../VEP_plugins/ \
+--plugin Downstream --plugin Wildtype  \
+--input_file ../../tests/TCGA.LUAD.mutect.hg38.somatic.vcf \
+--output_file ../../tests/TCGA-LUAD-mutect-hg38-VEP4pVACseq-somatic.vcf  
+
